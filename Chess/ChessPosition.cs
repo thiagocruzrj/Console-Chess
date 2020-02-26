9f39dc742cf07e6d0bd6806570cd655bc68f4177
Thiago Cruz <thagocruz@gmail.com>
2020-02-26 18:07:30 -0300

    Testing ChessPosition class

---
+++ Chess/ChessPosition.cs
@@ -0,0 +1,19 @@
+﻿namespace ConsoleChess.Chess
+{
+    class ChessPosition
+    {
+        public ChessPosition(char column, int line)
+        {
+            Column = column;
+            Line = line;
+        }
+
+        public char Column { get; set; }
+        public int Line { get; set; }
+
+        public override string ToString()
+        {
+            return "" + Column + Line;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,18 +8,8 @@ namespace ConsoleChess
     {
         static void Main(string[] args)
         {
-            try { 
-                BoardGame bor = new BoardGame(8, 8);
-
-                bor.PutPiece(new Tower(Color.Black, bor), new Position(0, 0));
-                bor.PutPiece(new Tower(Color.Black, bor), new Position(1, 3));
-                bor.PutPiece(new King(Color.Black, bor), new Position(0, 2));
-
-                Screen.PrintBoard(bor);
-            } catch (BoardException e)
-            {
-                Console.WriteLine(e.Message);
-            }
+            ChessPosition pos = new ChessPosition('a', 1);
+            Console.WriteLine(pos);
             Console.ReadLine();
         }
     }
